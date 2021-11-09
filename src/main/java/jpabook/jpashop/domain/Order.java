950d2b5970bd5257bb1d6e07d9f66f6ceeb00a4b
Ho-Lee <ho.sol.lee@gmail.com>
2021-11-09 14:18:01 +0900

    feat: @ManyToOne Fetch type LAZY로 변경, 연관관계 메소드 추가

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,11 +1,14 @@
 package jpabook.jpashop.domain;
 
+import static javax.persistence.FetchType.*;
+
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
@@ -26,7 +29,7 @@ public class Order {
 	@Column(name = "order_id")
 	private Long id;
 
-	@ManyToOne
+	@ManyToOne(fetch = LAZY)
 	@JoinColumn(name = "member_id")
 	private Member member;
 
@@ -40,4 +43,20 @@ public class Order {
 	private LocalDateTime orderDate; // 주문시간
 
 	private OrderStatus status; // 주문상태 [ORDER, CANCEL];
+
+	// 연관관계 메서드
+	public void setMember(Member member){
+		this.member = member;
+		member.getOrders().add(this);
+	}
+
+	public void addOrderItem(OrderItem orderItem){
+		orderItems.add(orderItem);
+		orderItem.setOrder(this);
+	}
+
+	public void setDelivery(Delivery delivery){
+		this.delivery = delivery;
+		delivery.setOrder(this);
+	}
 }
