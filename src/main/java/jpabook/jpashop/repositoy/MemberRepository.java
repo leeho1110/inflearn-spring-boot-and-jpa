75fb32e14cc687b64acf27e2169f66af94226491
Ho-Lee <ho.sol.lee@gmail.com>
2021-11-09 15:02:19 +0900

    feat: 회원 Service 개발 및 생성자 주입 방식을 통한 필드 인젝션

--- src/main/java/jpabook/jpashop/repositoy/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repositoy/MemberRepository.java
@@ -5,15 +5,19 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 
 @Repository
+@RequiredArgsConstructor // [3]
 public class MemberRepository {
 
-	@PersistenceContext
-	private EntityManager em;
+	// @PersistenceContext [1] 어노테이션을 통해 엔티티 매니저 주입
+	// @Autowired // [2] 스프링 데이터 JPA가 지원해줌
+	private final EntityManager em;
 
 	public void save(Member member) {
 		em.persist(member);
---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,53 @@
+package jpabook.jpashop.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repositoy.MemberRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional(readOnly = true)
+// @AllArgsConstructor : [2] 객체의 모든 필드에 대해서 생성자를 생성
+@RequiredArgsConstructor // [3] final 필드에 대해서 생성자를 생성
+public class MemberService {
+
+	private final MemberRepository memberRepository;
+
+	/*  [1] 생성자 주입 방식
+	   	@Autowired
+	 	public MemberService(MemberRepository memberRepository) {
+	 	this.memberRepository = memberRepository;
+	 } */
+
+	// 회원 가입
+	@Transactional
+	public Long join(Member member){
+		validateDuplicateMember(member);
+		memberRepository.save(member);
+
+		return member.getId();
+	}
+
+	private void validateDuplicateMember(Member member) {
+		if(isDuplicatedMember(member)) {
+			throw new IllegalStateException("이미 존재하는 회원입니다.");
+		}
+	}
+
+	private boolean isDuplicatedMember(Member member) {
+		return !memberRepository.findByName(member.getName()).isEmpty();
+	}
+
+	// 회원 전체 조회 (읽기에는 readOnly=true 를 넣어서 성능 최적화, 최상단 @Transactional 따라감)
+	public List<Member> finMembers(){
+		return memberRepository.findAll();
+	}
+
+	public Member findOne(Long memberId){
+		return memberRepository.findOne(memberId);
+	}
+}
