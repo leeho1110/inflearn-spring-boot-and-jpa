018f89167f088eeb238f30bfa554b2401ed19b8c
Ho-Lee <ho.sol.lee@gmail.com>
2021-11-10 13:46:21 +0900

    chore: 미사용 라이브러리 import 구문 삭제

--- src/main/java/jpabook/jpashop/repositoy/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repositoy/OrderRepository.java
@@ -1,11 +1,7 @@
 package jpabook.jpashop.repositoy;
 
-import java.util.List;
-import java.util.OptionalDouble;
-
 import javax.persistence.EntityManager;
 
-import org.aspectj.weaver.ast.Or;
 import org.springframework.stereotype.Repository;
 
 import jpabook.jpashop.domain.Order;
