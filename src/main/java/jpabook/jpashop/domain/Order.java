830c97874d0b8c59d641f188f5694e3361e01d08
Ho-Lee <ho.sol.lee@gmail.com>
2021-11-10 16:28:51 +0900

    fix: 주문상품의 총합이 아닌 상품가격만 더해지는 버그 수정

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -93,7 +93,7 @@ public class Order {
 
 	// 조회 로직 : 전체 주문 가격 조회
 	public int getTotalPrice() {
-		return orderItems.stream().mapToInt(OrderItem::getOrderPrice).sum();
+		return orderItems.stream().mapToInt(OrderItem::getTotalPrice).sum();
 	}
 
 }
