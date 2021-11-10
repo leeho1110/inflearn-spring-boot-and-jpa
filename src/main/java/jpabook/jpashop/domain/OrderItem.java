70c41297ac4a6e928fbf0184661215ba9efca7d1
Ho-Lee <ho.sol.lee@gmail.com>
2021-11-10 13:42:04 +0900

    feat: 주문, 주문상품 엔티티 개발

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -4,7 +4,6 @@ import static javax.persistence.FetchType.*;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
@@ -34,4 +33,23 @@ public class OrderItem {
 	private int orderPrice;
 	private int count;
 
+	// 생성 메서드
+	public static OrderItem createOrderItem(Item item, int orderPrice, int count){
+		OrderItem orderItem = new OrderItem();
+		orderItem.setItem(item);
+		orderItem.setOrderPrice(orderPrice);
+		orderItem.setCount(count);
+
+		item.removeStock(count);
+		return orderItem;
+	}
+
+	// 비즈니스 로직
+	public void cancel() {
+		getItem().addStock(count);
+	}
+
+	public int getTotalPrice() {
+		return getOrderPrice() * getCount ();
+	}
 }
