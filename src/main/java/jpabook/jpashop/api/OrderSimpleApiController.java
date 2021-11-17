d11d2582b0803e90b542d1c9a1ccbd489cb8c6d6
Ho-Lee <ho.sol.lee@gmail.com>
2021-11-17 22:03:43 +0900

    feat: Hibernate5 모듈을 사용하지 않고 지연로딩이 설정된 필드에 getter를 통해 값 세팅
    See also: f31e833

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -24,7 +24,11 @@ public class OrderSimpleApiController {
 	@GetMapping("/api/v1/simple-orders")
 	public List<Order> ordersV1(){
 		List<Order> all = orderRepository.findAllByString(new OrderSearch());
-
+		for (Order order : all) {
+			// LAZY 로딩 강제 초기화
+			order.getMember().getName();
+			order.getDelivery().getAddress();
+		}
 		return all;
 	}
 }
