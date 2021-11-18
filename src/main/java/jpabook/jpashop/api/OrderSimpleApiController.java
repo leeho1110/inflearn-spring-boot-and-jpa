2c3c006ee1f01da3790f89f4ec40040f0868afab
Ho-Lee <ho.sol.lee@gmail.com>
2021-11-18 23:53:33 +0900

    feat: 엔티티를 그대로 반환하는 것이 아닌 DTO로 변환하여 반환

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,13 +1,18 @@
 package jpabook.jpashop.api;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderSearch;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repositoy.OrderRepository;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
 
@@ -31,4 +36,29 @@ public class OrderSimpleApiController {
 		}
 		return all;
 	}
+
+	@GetMapping("/api/v2/simple-orders")
+	public List<SimpleOrderDto> ordersV2(){
+		return orderRepository.findAllByString(new OrderSearch()).stream()
+			.map(SimpleOrderDto::new)
+			.collect(Collectors.toList());
+	}
+	
+	@Data
+	static class SimpleOrderDto {
+		private Long orderId;
+		private String name;
+		private LocalDateTime orderDate;
+		private OrderStatus orderStatus;
+		private Address address;
+
+		public SimpleOrderDto(Order order) {
+			this.orderId = order.getId();
+			this.name = order.getMember().getName();
+			this.orderDate = order.getOrderDate();
+			this.orderStatus = order.getStatus();
+			this.address = order.getDelivery().getAddress();
+		}
+	}
+	
 }
