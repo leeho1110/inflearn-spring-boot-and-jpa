d4ef12450b66bf1a6dd0cc2945513f66e3a0bffc
Ho-Lee <ho.sol.lee@gmail.com>
2021-11-22 22:14:41 +0900

    feat: feat: 주문 조회 V3.1 - Fetch 조인의 단점인 페이징 불가를 개선 [2]
    v3의 fetch 조인은 한번에 (Query 1번) 데이터를 가져온다. 따라서 중복된 데이터가 한꺼번에 애플리케이션으로 전달되고,
    데이터 전송량이 경우에 따라 매우 방대해질 수 있다.
    
    default_batch_fetch_size 를 설정하면 같은 예시에서는 쿼리 횟수는 비록 1 -> 3으로 증가했다.
    반면에 LAZY 로딩의 강제 초기화에서 발생할 수 있는 1*N*M 의 횟수를 1+1+1 로 줄였고 중복도 사라지며 필요한 데이터만 정확하게 가져올 수 있다.
    그리고 offset, limit 을 @RequestParam을 통해 전달받아 페이징이 가능하다.

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import jpabook.jpashop.domain.Address;
@@ -52,8 +53,11 @@ public class OrderApiController {
 	}
 
 	@GetMapping("/api/v3.1/orders")
-	public List<OrderDto> ordersV3_page() {
-		List<Order> orders = orderRepository.findAllWithMemberDelivery();
+	public List<OrderDto> ordersV3_page(
+			@RequestParam(value = "offset", defaultValue = "0") int offset,
+			@RequestParam(value = "limit", defaultValue = "100") int limit)
+	{
+		List<Order> orders = orderRepository.findAllWithMemberDelivery(offset,limit);
 
 		List<OrderDto> result = orders.stream()
 			.map(order -> new OrderDto(order))
