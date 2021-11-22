c509375c27c0903c35072c00682c6fff8904c38b
Ho-Lee <ho.sol.lee@gmail.com>
2021-11-22 14:55:25 +0900

    feat: 주문 조회 V2 - 엔티티를 DTO로 변환하여 Response
    하지만 이 방법은 결국 LAZY 로딩의 강제 초기화때마다 쿼리가 나간다. 따라서 성능울 위해 Fetch 조인을 활용한 최적화가 필요하다.

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -1,15 +1,19 @@
 package jpabook.jpashop.api;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
-import org.apache.catalina.LifecycleState;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.OrderSearch;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repositoy.OrderRepository;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -30,5 +34,49 @@ public class OrderApiController {
 			orderItems.stream().forEach(orderItem -> orderItem.getItem().getName());
 		}
 		return all;
+	}
+
+	@GetMapping("/api/v2/orders")
+	public List<OrderDto> ordersV2(){
+		List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+		return orders.stream()
+			.map(order -> new OrderDto(order))
+			.collect(Collectors.toList());
+	}
+
+	@Getter
+	static class OrderDto {
+
+		private Long orderId;
+		private String name;
+		private LocalDateTime orderDate;
+		private OrderStatus status;
+		private Address address;
+		private List<OrderItemDto> orderItems;
+
+		public OrderDto(Order order) {
+			this.orderId = order.getId();
+			this.name = order.getMember().getName();
+			this.orderDate = order.getOrderDate();
+			this.status = order.getStatus();
+			this.address = order.getDelivery().getAddress();
+			this.orderItems = order.getOrderItems().stream()
+				.map(orderItem -> new OrderItemDto(orderItem))
+				.collect(Collectors.toList());
+		}
+	}
+
+	@Getter
+	static class OrderItemDto {
+
+		private String itemName;
+		private int orderPrice;
+		private int count;
+
+		public OrderItemDto(OrderItem orderItem) {
+			this.itemName = orderItem.getItem().getName();
+			this.orderPrice = orderItem.getOrderPrice();
+			this.count = orderItem.getCount();
+		}
 	}
 }
